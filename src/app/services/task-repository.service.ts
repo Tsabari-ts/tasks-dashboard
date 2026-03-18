8592704f72d28912a470560da6cc1d32368e9ee2
Tsabari-ts <101357656+Tsabari-ts@users.noreply.github.com>
2026-03-18 14:26:17 +0200

    Update task-repository.service.ts

--- src/app/services/task-repository.service.ts
+++ src/app/services/task-repository.service.ts
@@ -12,18 +12,18 @@ export class TaskRepositoryService {
   constructor(private http:HttpClient) { }
 
   getAllTasks() {
-    return this.http.get<TaskItem[]>(`https://localhost:7176/Tasks/GetAllTasks`);
+    return this.http.get<TaskItem[]>(`http://localhost:7176/Tasks/GetAllTasks`);
   }
 
   createTask(taskData: TaskItem): Observable<OperationResult> {
-    return this.http.post<any>(`https://localhost:7176/Tasks/CreateTask/`, taskData);
+    return this.http.post<any>(`http://localhost:7176/Tasks/CreateTask/`, taskData);
   }
 
   updateTask(taskId: number, taskData: TaskItem): Observable<OperationResult> {
-    return this.http.put<any>(`https://localhost:7176/Tasks/UpdateTask/${taskId}`, taskData);
+    return this.http.put<any>(`http://localhost:7176/Tasks/UpdateTask/${taskId}`, taskData);
   }
 
   deleteTaskById(taskId:number): Observable<OperationResult> {
-    return this.http.delete<any>(`https://localhost:7176/Tasks/DeleteTask/${taskId}`);
+    return this.http.delete<any>(`http://localhost:7176/Tasks/DeleteTask/${taskId}`);
   }
 }
